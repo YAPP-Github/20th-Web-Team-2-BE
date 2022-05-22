80b9855d5fa9433669e3a4bc9fd9c7196fa1ff06
bean.kong <bean.kong@kakaocorp.com>
2022-05-22 23:03:45 +0900

    project 초기 셋팅 완료

---
+++ config-service/src/test/java/com/isloand/configservice/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isloand.configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ matching-service/src/test/java/com/isloand/matchingservice/MatchingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isloand.matchingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
