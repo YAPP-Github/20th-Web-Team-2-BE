80393e26486703848c4aa9a5032dc22782c6622a
rlacksgus97 <rlacksgus97@naver.com>
2022-05-26 16:00:31 +0900

    [feat] : h2 데이터베이스 연결, User 엔티티 생성

---
+++ user-service/src/main/java/com/isloand/userservice/domain/User.java
@@ -0,0 +1,18 @@
+package com.isloand.userservice.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    private Long id;
+
+    private String kakaoEmail;
+
+    private String universityEmail;
+
+    private String university;
+
+    private boolean isAuthenticated;
+}
