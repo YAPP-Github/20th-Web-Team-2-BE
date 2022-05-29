d6c62714114a63321abb5238462c6b4b09bd5fab
bean.kong <bean.kong@kakaocorp.com>
2022-05-30 01:24:47 +0900

    불필요한 코드 제거

--- user-service/src/main/java/com/isloand/userservice/controller/OAuthController.java
+++ user-service/src/main/java/com/isloand/userservice/controller/OAuthController.java
@@ -10,9 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/oauth")
