b9e99e0a1b786c7fc6ffcf8418402675ef9df265
rlacksgus97 <rlacksgus97@naver.com>
2022-06-03 10:32:26 +0900

    [feat] : builder, getter 설정

--- user-service/src/main/java/com/isloand/userservice/entity/EmailTokenEntity.java
+++ user-service/src/main/java/com/isloand/userservice/entity/EmailTokenEntity.java
@@ -1,11 +1,18 @@
 package com.isloand.userservice.entity;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@Builder
+@NoArgsConstructor
 public class EmailTokenEntity {
     @Id
     private Long id;
