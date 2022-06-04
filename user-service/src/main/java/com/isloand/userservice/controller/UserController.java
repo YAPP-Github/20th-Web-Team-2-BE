8e9da3356971869ac25745fb4f35f0c7bf3e266c
bean.kong <bean.kong@kakaocorp.com>
2022-06-04 21:58:30 +0900

    token 바탕 사용자id 얻어오기 기능 구현

--- user-service/src/main/java/com/isloand/userservice/controller/UserController.java
+++ user-service/src/main/java/com/isloand/userservice/controller/UserController.java
@@ -1,12 +1,11 @@
 package com.isloand.userservice.controller;
 
+import com.isloand.userservice.client.KakaoApiClient;
+import com.isloand.userservice.dto.KakaoTokenInfoResponse;
 import com.isloand.userservice.service.EmailService;
 import com.isloand.userservice.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
@@ -15,6 +14,7 @@ public class UserController {
 
     private final UserService userService;
     private final EmailService emailService;
+    private final KakaoApiClient kakaoApiClient;
 
     @GetMapping("/email")
     public void sendEmail() {
@@ -30,5 +30,11 @@ public class UserController {
     @PostMapping("/email")
     public void authenticateByEmail(String authCode) {
         emailService.authenticateByEmail(authCode);
+    }
+
+    //token 바탕 사용자 id 얻어오는 사용 예시
+    @GetMapping("/id")
+    public KakaoTokenInfoResponse getKakaoId(@RequestHeader(value = "Authorization") String token) {
+        return kakaoApiClient.getTokenInfo(token);
     }
 }
