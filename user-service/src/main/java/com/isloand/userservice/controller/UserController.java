eca7f430e7a62c597aed8cf9d4d9a5da0aa3dc63
bean.kong <bean.kong@kakaocorp.com>
2022-06-04 22:49:43 +0900

    임시 api 생성 및 api url 변경

--- user-service/src/main/java/com/isloand/userservice/controller/UserController.java
+++ user-service/src/main/java/com/isloand/userservice/controller/UserController.java
@@ -3,18 +3,22 @@ package com.isloand.userservice.controller;
 import com.isloand.userservice.client.KakaoApiClient;
 import com.isloand.userservice.dto.KakaoTokenInfoResponse;
 import com.isloand.userservice.service.EmailService;
+import com.isloand.userservice.service.UniversityService;
 import com.isloand.userservice.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
     private final EmailService emailService;
     private final KakaoApiClient kakaoApiClient;
+    private final UniversityService universityService;
 
     @GetMapping("/email")
     public void sendEmail() {
@@ -32,9 +36,17 @@ public class UserController {
         emailService.authenticateByEmail(authCode);
     }
 
+    //TODO : 이후에 제거
     //token 바탕 사용자 id 얻어오는 사용 예시
     @GetMapping("/id")
     public KakaoTokenInfoResponse getKakaoId(@RequestHeader(value = "Authorization") String token) {
         return kakaoApiClient.getTokenInfo(token);
+    }
+
+    //TODO : 이후에 제거
+    //대학교 정보 등록용 임시 api
+    @PostMapping("/university")
+    public void registerUniversityInfo() throws IOException {
+        universityService.registerUniInfo();
     }
 }
