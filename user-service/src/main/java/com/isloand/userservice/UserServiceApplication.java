bd0192011fa33f0fd132a4ccd36819e7da2ea49c
rlacksgus97 <rlacksgus97@naver.com>
2022-06-05 05:43:22 +0900

    [feat] : user-service eureka client 설정

--- user-service/src/main/java/com/isloand/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/isloand/userservice/UserServiceApplication.java
@@ -2,9 +2,11 @@ package com.isloand.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableFeignClients
 public class UserServiceApplication {
 
