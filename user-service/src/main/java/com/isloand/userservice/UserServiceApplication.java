d5f189f3da76dc012521d27503dbcffffeb2ad20
rlacksgus97 <rlacksgus97@naver.com>
2022-06-09 10:46:03 +0900

    [fix] : @EnableDiscoveryClient

--- user-service/src/main/java/com/isloand/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/isloand/userservice/UserServiceApplication.java
@@ -2,11 +2,12 @@ package com.isloand.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableFeignClients
 public class UserServiceApplication {
 
