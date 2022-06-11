21d3e6ce1dee42ee84650a6c207700328eb8889a
rlacksgus97 <rlacksgus97@naver.com>
2022-06-11 14:26:23 +0900

    [feat] : global swagger 적용

--- user-service/src/main/java/com/isloand/userservice/controller/UserController.java
+++ user-service/src/main/java/com/isloand/userservice/controller/UserController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.io.IOException;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/user")
 @RequiredArgsConstructor
 public class UserController {
 
