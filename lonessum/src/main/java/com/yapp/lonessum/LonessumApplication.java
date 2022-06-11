d53ee5645169587c58cf33d7fb68050f1df73142
rlacksgus97 <rlacksgus97@naver.com>
2022-06-11 17:14:52 +0900

    [feat] : 메인 프로젝트 생성

---
+++ lonessum/src/main/java/com/yapp/lonessum/LonessumApplication.java
@@ -0,0 +1,13 @@
+package com.yapp.lonessum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LonessumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LonessumApplication.class, args);
+	}
+
+}
