f44165d785e07165c8a3eca59d65f655c2e58a22
bean.kong <bean.kong@kakaocorp.com>
2022-06-15 01:00:02 +0900

    meeting matching algorithm 구현

---
+++ lonessum/src/test/java/com/yapp/lonessum/domain/meeting/algorithm/MeetingMatchingAlgorithmTest.java
@@ -0,0 +1,29 @@
+package com.yapp.lonessum.domain.meeting.algorithm;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static com.yapp.lonessum.domain.meeting.utils.testDataFactory.getTestData;
+
+class MeetingMatchingAlgorithmTest {
+    @Test
+    @DisplayName("매칭 알고리즘 최적의 결과 도출 테스트")
+    public void algorithmTest() {
+        MeetingMatchingAlgorithm meetingMatchingAlgorithm = new MeetingMatchingAlgorithm();
+
+        List<MeetingSurveyDto> testData = getTestData();
+        List<MatchingInfo<MeetingSurveyDto>> result = meetingMatchingAlgorithm.getResult(testData);
+
+        MeetingSurveyDto first = result.get(0).getFirst();
+        System.out.println("first = " + first);
+        MeetingSurveyDto second = result.get(0).getSecond();
+        System.out.println("second = " + second);
+
+        Assertions.assertThat(1).isEqualTo(result.size());
+        Assertions.assertThat(-23).isEqualTo(result.get(0).getScore());
+    }
+
+}
