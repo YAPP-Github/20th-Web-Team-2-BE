c4cb5a61ee736f99e9840acbe1f419ffd4a9858d
rlacksgus97 <rlacksgus97@naver.com>
2022-06-16 00:04:47 +0900

    [chore] : 불필요한 import 삭제

--- lonessum/src/main/java/com/yapp/lonessum/mapper/MeetingSurveyMapper.java
+++ lonessum/src/main/java/com/yapp/lonessum/mapper/MeetingSurveyMapper.java
@@ -3,8 +3,6 @@ package com.yapp.lonessum.mapper;
 import com.yapp.lonessum.domain.meeting.dto.MeetingSurveyDto;
 import com.yapp.lonessum.domain.meeting.entity.MeetingSurveyEntity;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface MeetingSurveyMapper extends GenericMapper<MeetingSurveyDto, MeetingSurveyEntity>{
