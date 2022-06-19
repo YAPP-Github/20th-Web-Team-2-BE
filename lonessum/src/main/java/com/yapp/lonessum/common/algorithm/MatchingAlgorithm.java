d94fd638bf43e8cb8da040df84daab3b24b833af
bean.kong <bean.kong@kakaocorp.com>
2022-06-19 17:36:17 +0900

    추상화 및 공통화 작업

--- lonessum/src/main/java/com/yapp/lonessum/common/algorithm/MatchingAlgorithm.java
+++ lonessum/src/main/java/com/yapp/lonessum/common/algorithm/MatchingAlgorithm.java
@@ -1,14 +1,59 @@
 package com.yapp.lonessum.common.algorithm;
 
+import com.yapp.lonessum.common.dto.SurveyDto;
 import org.springframework.data.util.Pair;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public abstract class MatchingAlgorithm<T> {
-    public List<MatchingInfo<T>> getResult(List<T> list) {
-        return null;
+    public List<MatchingInfo<T>> getResult(List<T> surveyList) {
+        List<MatchingInfo<T>> result = new ArrayList<>();
+
+        boolean[] visited = new boolean[surveyList.size()];
+        Arrays.fill(visited, Boolean.FALSE);
+
+        List<Pair<Integer, Integer>> cases = new ArrayList<>();
+
+        getAllCases(visited, 0, surveyList.size(), 2, cases);
+
+        for (Pair<Integer, Integer> aCase : cases) {
+            int firstIdx = aCase.getFirst();
+            int secondIdx = aCase.getSecond();
+
+            T firstSurvey = surveyList.get(firstIdx);
+            T secondSurvey = surveyList.get(secondIdx);
+
+            int score = calAllCasesScore(firstSurvey, secondSurvey);
+            MatchingInfo<T> matchingInfo = new MatchingInfo(score, firstSurvey, secondSurvey);
+            result.add(matchingInfo);
+        }
+
+        return calOptimalMatchingCase(result);
     }
+
+    public List<MatchingInfo<T>> calOptimalMatchingCase(List<MatchingInfo<T>> cases) {
+        List<MatchingInfo<T>> result = new ArrayList<>();
+        Map<Long, Boolean> matchingMap = new HashMap<>();
+
+        Collections.sort(cases);
+
+        for (MatchingInfo<T> m : cases) {
+            Long id1 = ((SurveyDto)m.getFirst()).getId();
+            Long id2 = ((SurveyDto)m.getSecond()).getId();
+
+            if (matchingMap.containsKey(id1) || matchingMap.containsKey(id2)) {
+                continue;
+            }
+            matchingMap.put(id1, true);
+            matchingMap.put(id2, true);
+
+            result.add(m);
+        }
+
+        return result;
+    }
+
+    public abstract <T> int calAllCasesScore(T first, T second);
 
     public void getAllCases(boolean[] visited, int start, int n, int r, List<Pair<Integer, Integer>> cases) {
         if (r == 0) {
