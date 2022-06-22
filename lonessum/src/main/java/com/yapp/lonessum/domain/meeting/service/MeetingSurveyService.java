2c14aeecf0d1649adb3f4952924b671851cb7e7b
rlacksgus97 <rlacksgus97@naver.com>
2022-06-22 10:53:36 +0900

    [feat] : 설문 생성하기 전 이메일 인증된 유저인지 검증

--- lonessum/src/main/java/com/yapp/lonessum/domain/meeting/service/MeetingSurveyService.java
+++ lonessum/src/main/java/com/yapp/lonessum/domain/meeting/service/MeetingSurveyService.java
@@ -24,9 +24,14 @@ public class MeetingSurveyService {
     public Long createSurvey(MeetingSurveyDto meetingSurveyDto) {
         // 토큰에서 userId 가져옴
         Long userId = null;
-//        UserEntity user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("존재하지 않는 유저입니다."));
+        UserEntity user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("존재하지 않는 유저입니다."));
+
+        if (!user.getIsAuthenticated()) {
+            throw new RuntimeException("이메일 인증이 완료되지 않은 유저입니다.");
+        }
+
         MeetingSurveyEntity meetingSurvey = meetingSurveyMapper.toEntity(meetingSurveyDto);
-//        meetingSurvey.setUser(user);
+        meetingSurvey.setUser(user);
         return meetingSurveyRepository.save(meetingSurvey).getId();
     }
 
