3f3a255ca20b2ece4b239d930b242b43f83623a2
rlacksgus97 <rlacksgus97@naver.com>
2022-06-24 14:33:46 +0900

    [fix] : averageHeight 자료형 변경

--- lonessum/src/main/java/com/yapp/lonessum/domain/meeting/dto/MeetingSurveyDto.java
+++ lonessum/src/main/java/com/yapp/lonessum/domain/meeting/dto/MeetingSurveyDto.java
@@ -23,7 +23,7 @@ public class MeetingSurveyDto extends SurveyDto {
 
     private List<Department> ourDepartments;
 
-    private Long averageHeight;
+    private List<Long> averageHeight;
 
     private List<Long> avoidUniversities;
 
