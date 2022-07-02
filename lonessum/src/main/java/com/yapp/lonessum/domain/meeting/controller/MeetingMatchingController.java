d5f2f3bdc103f8c24970acee546ea81bc09ce0fe
rlacksgus97 <rlacksgus97@naver.com>
2022-07-02 19:37:13 +0900

    [feat] : 매칭 상태를 조회하는 API(미완료)

---
+++ lonessum/src/main/java/com/yapp/lonessum/domain/meeting/controller/MeetingMatchingController.java
@@ -0,0 +1,26 @@
+package com.yapp.lonessum.domain.meeting.controller;
+
+import com.yapp.lonessum.domain.meeting.service.MeetingMatchingService;
+import com.yapp.lonessum.domain.user.entity.UserEntity;
+import com.yapp.lonessum.domain.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/meeting/matching")
+public class MeetingMatchingController {
+
+    private final UserService userService;
+    private final MeetingMatchingService meetingmatchingService;
+
+    @GetMapping
+    public ResponseEntity getMatchResult(@RequestHeader(value = "Authorization") String token) {
+        UserEntity user = userService.getUserFromToken(token);
+        return ResponseEntity.ok(meetingmatchingService.getMatchResult(user));
+    }
+}
