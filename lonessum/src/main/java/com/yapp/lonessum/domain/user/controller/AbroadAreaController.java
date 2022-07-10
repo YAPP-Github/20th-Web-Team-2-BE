7def372729d7637e91274c837d6985850c2da20d
bean.kong <bean.kong@kakaocorp.com>
2022-07-10 21:12:58 +0900

    지역정보 등록 및 조회 api 생성

---
+++ lonessum/src/main/java/com/yapp/lonessum/domain/user/controller/AbroadAreaController.java
@@ -0,0 +1,31 @@
+package com.yapp.lonessum.domain.user.controller;
+
+import com.yapp.lonessum.domain.user.dto.AbroadAreaDto;
+import com.yapp.lonessum.domain.user.dto.UniversityDto;
+import com.yapp.lonessum.domain.user.service.AbroadAreaService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.List;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/areas")
+@RequiredArgsConstructor
+public class AbroadAreaController {
+    private final AbroadAreaService abroadAreaService;
+
+    @GetMapping
+    public ResponseEntity<List<AbroadAreaDto>> getAllAreas() {
+        return ResponseEntity.ok(abroadAreaService.getAllAbroadAreas());
+    }
+
+    //TODO : 이후에 제거
+    //지역 정보 등록용 임시 api
+    @PostMapping()
+    public void registerAbroadAreaInfo() throws IOException {
+        abroadAreaService.registerUniInfo();
+    }
+}
--- lonessum/src/main/java/com/yapp/lonessum/domain/user/controller/UserController.java
+++ lonessum/src/main/java/com/yapp/lonessum/domain/user/controller/UserController.java
@@ -2,11 +2,8 @@ package com.yapp.lonessum.domain.user.controller;
 
 import com.yapp.lonessum.domain.user.client.KakaoApiClient;
 import com.yapp.lonessum.domain.user.dto.KakaoTokenInfoResponse;
-import com.yapp.lonessum.domain.user.service.UniversityService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-
-import java.io.IOException;
 
 
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -16,19 +13,11 @@ import java.io.IOException;
 public class UserController {
 
     private final KakaoApiClient kakaoApiClient;
-    private final UniversityService universityService;
 
     //TODO : 이후에 제거
     //token 바탕 사용자 id 얻어오는 사용 예시
     @GetMapping("/id")
     public KakaoTokenInfoResponse getKakaoId(@RequestHeader(value = "Authorization") String token) {
         return kakaoApiClient.getTokenInfo(token);
-    }
-
-    //TODO : 이후에 제거
-    //대학교 정보 등록용 임시 api
-    @PostMapping("/university")
-    public void registerUniversityInfo() throws IOException {
-        universityService.registerUniInfo();
     }
 }
