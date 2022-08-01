0b72b8c29cb42499d1c923614b59d9a19b453e9d
rlacksgus97 <rlacksgus97@naver.com>
2022-08-01 18:44:28 +0900

    resolve merge conflict

--- lonessum/src/main/java/com/yapp/lonessum/domain/payment/entity/PaymentEntity.java
+++ lonessum/src/main/java/com/yapp/lonessum/domain/payment/entity/PaymentEntity.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 @Getter
 @Builder
 @NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class PaymentEntity {
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
