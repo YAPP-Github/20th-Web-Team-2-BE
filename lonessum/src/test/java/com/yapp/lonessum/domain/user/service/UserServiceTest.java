a36bb4fad9614568e444277e50040bfe5d15c9bc
rlacksgus97 <rlacksgus97@naver.com>
2022-08-01 17:44:28 +0900

    [test] : 매칭이 성사된 후 탈퇴한 유저의 상대방 설문의 매칭 상태를 수정

--- lonessum/src/test/java/com/yapp/lonessum/domain/user/service/UserServiceTest.java
+++ lonessum/src/test/java/com/yapp/lonessum/domain/user/service/UserServiceTest.java
@@ -1,6 +1,7 @@
 package com.yapp.lonessum.domain.user.service;
 
 import com.yapp.lonessum.domain.constant.Gender;
+import com.yapp.lonessum.domain.constant.MatchStatus;
 import com.yapp.lonessum.domain.meeting.entity.MeetingMatchingEntity;
 import com.yapp.lonessum.domain.meeting.entity.MeetingSurveyEntity;
 import com.yapp.lonessum.domain.meeting.repository.MeetingMatchingRepository;
@@ -36,12 +37,13 @@ class UserServiceTest {
     EntityManager entityManager;
 
     @Test
+    @Transactional
     public void 회원탈퇴_테스트() {
         //given
-        userRepository.save(UserEntity.builder().id(1L).build());
+        UserEntity user = userRepository.save(UserEntity.builder().build());
 
         //when
-        userService.withdraw(1L);
+        userService.withdraw(user.getId());
 
         //then
         Assertions.assertThat(userRepository.findById(1L).isEmpty()).isEqualTo(true);
@@ -52,14 +54,14 @@ class UserServiceTest {
     public void 여자_탈퇴_시_해당_매칭은_환불이_필요하다() {
         //given
         //남자 설문
-        MeetingSurveyEntity maleSurvey = MeetingSurveyEntity.builder().gender(Gender.MALE).build();
+        MeetingSurveyEntity maleSurvey = MeetingSurveyEntity.builder().gender(Gender.MALE).matchStatus(MatchStatus.MATCHED).build();
         maleSurvey = meetingSurveyRepository.save(maleSurvey);
 
         //남자 유저
         UserEntity male = UserEntity.builder().meetingSurvey(maleSurvey).build();
 
         //여자 설문
-        MeetingSurveyEntity femaleSurvey = MeetingSurveyEntity.builder().gender(Gender.FEMALE).build();
+        MeetingSurveyEntity femaleSurvey = MeetingSurveyEntity.builder().gender(Gender.FEMALE).matchStatus(MatchStatus.PAID).build();
         femaleSurvey = meetingSurveyRepository.save(femaleSurvey);
 
         //여자 유저
@@ -80,6 +82,9 @@ class UserServiceTest {
                 .payment(payment)
                 .build();
 
+        maleSurvey.changeMeetingMatching(meetingMatching);
+        femaleSurvey.changeMeetingMatching(meetingMatching);
+
         meetingMatchingRepository.save(meetingMatching);
 
         //when
@@ -89,6 +94,8 @@ class UserServiceTest {
         PaymentEntity result = paymentRepository.findById(payment.getId()).get();
         Assertions.assertThat(result.getIsNeedRefund())
                 .isEqualTo(true);
+        Assertions.assertThat(meetingMatching.getMaleSurvey().getMatchStatus())
+                .isEqualTo(MatchStatus.NEED_REFUND);
     }
 
     @Test
@@ -124,6 +131,9 @@ class UserServiceTest {
                 .payment(payment)
                 .build();
 
+        maleSurvey.changeMeetingMatching(meetingMatching);
+        femaleSurvey.changeMeetingMatching(meetingMatching);
+
         meetingMatchingRepository.save(meetingMatching);
 
         //when
@@ -133,5 +143,7 @@ class UserServiceTest {
         PaymentEntity result = paymentRepository.findById(payment.getId()).get();
         Assertions.assertThat(result.getIsNeedRefund())
                 .isEqualTo(false);
+        Assertions.assertThat(meetingMatching.getFemaleSurvey().getMatchStatus())
+                .isEqualTo(MatchStatus.FAILED);
     }
 }
