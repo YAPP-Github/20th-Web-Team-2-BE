21f728637ad37f3753343863e0b6c9284dcf932f
rlacksgus97 <rlacksgus97@naver.com>
2022-08-04 01:34:38 +0900

    [fix] : @NoArgsConstructor 추가

--- lonessum/src/main/java/com/yapp/lonessum/domain/admin/PaymentDto.java
+++ lonessum/src/main/java/com/yapp/lonessum/domain/admin/PaymentDto.java
@@ -2,8 +2,10 @@ package com.yapp.lonessum.domain.admin;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class PaymentDto {
     private String kakaoId;
